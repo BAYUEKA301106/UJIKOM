95dfb81444cd743fb5c11ec39bd81ef03b90fbc3
BAYUEKA301106 <muhammadbayuekaputra9@gmail.com>
2024-02-06 08:19:38 +0700

    first commit

---
+++ app/Http/Controllers/bukucontroller.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\buku;
+use App\Http\Controllers\controller;
+use Illuminate\Http\Request;
+
+class bukucontroller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $buku = buku::all();
+        return view('buku.index',['buku'=>$buku]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view ('buku.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function show(buku $buku)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(buku $buku)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, buku $buku)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(buku $buku)
+    {
+        //
+    }
+}
